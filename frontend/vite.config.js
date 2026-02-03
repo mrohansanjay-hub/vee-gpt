89d9bee06f536f920a4b2d3b1ee0ccf3eb33ffe4
mrohansanjay-hub <mrohansanjay@gmail.com>
2026-02-03 23:42:51 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      'k8s-default-appingre-5c306dac3d-400918894.us-east-1.elb.amazonaws.com',
-    ],
+    allowedHosts: 'all',
   },
 })
